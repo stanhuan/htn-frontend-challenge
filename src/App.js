a568fb52720b4f80c89c4039220c1f92dc39c0c7
Stanley Huang <stanley@Stanleys-MacBook-Pro-2.local>
2018-02-17 14:45:34 -0500

    Prep for css styles, added normalize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Home, Schedule } from './pages';
+import 'normalize.css';
 import './App.css';
 
 class App extends Component {
